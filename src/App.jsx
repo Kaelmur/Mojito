ad18f4f67f90874d8af4a4bf34fb137fc01cf653
Kaelmur <a.murataliev@list.ru>
2025-07-18 14:37:17 +0500

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-  return <div className="flex-center h-[100vh]">Hello</div>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black"></div>
+    </main>
+  );
 }
 
 export default App;
